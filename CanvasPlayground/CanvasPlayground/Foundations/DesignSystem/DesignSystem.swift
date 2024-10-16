39023e9cd6e16f71901f88bf9d41d38a44f7fc43
Moein Barzegaran <barzegaranmoein@gmail.com>
2024-10-16 15:36:27 +0200

    Added Basic colors and design system structure

---
+++ CanvasPlayground/CanvasPlayground/Foundations/DesignSystem/DesignSystem.swift
@@ -0,0 +1,20 @@
+//
+//  DesignSystem.swift
+//  CanvasPlayground
+//
+//  Created by Moein Barzegaran on 10/16/24.
+//
+
+import UIKit
+import SwiftUI
+
+struct DesignSystem {
+    struct Size {}
+
+    struct Paddings {}
+}
+
+extension UIColor {
+    static let mainBackground = UIColor(named: "background")!
+    static let bgSecondary = UIColor(named: "backgroundSecondary")!
+}
